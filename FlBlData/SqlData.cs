705bb3abf1c451e682b33660b3fc84c7eb3c76e0
Kyla Aquino <116413309+KylaKurstienAquino@users.noreply.github.com>
2023-07-08 13:34:10 +0800

    Create SqlData.cs

---
+++ FlBlData/SqlData.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace FlBlData
+{
+    public class SqlData
+    {
+        //local path or file path
+        string connectionString
+             = "Data Source=MIYUAKI\SQLEXPRESS;Initial Catalog = followblock; Integrated Security = True:";
+
+        //initializing sql connection
+        static SqlConnection connection;
+
+        public SqlData()
+        {
+            connection = new SqlConnection(connectionString);
+        }
+
+
+    }
+}
