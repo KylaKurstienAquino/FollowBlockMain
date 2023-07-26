62eb8f2590d7c7c5ad3004baf840e60777a4d1f3
Claris Batacandolo <102318568+Yhesa@users.noreply.github.com>
2023-07-26 17:30:02 +0800

    Update BlockedAccounts.cs

--- FlBlModel/BlockedAccounts.cs
+++ FlBlModel/BlockedAccounts.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +6,32 @@ using System.Threading.Tasks;
 
 namespace FlBlModel
 {
-    public class BlockedAccounts
+    public class BlockededAccounts
     {
-        public string blocked { get; set; }
+        public BlockedAccounts(string blockerUser, string studentUser)
+        {
+            BlockerUser = blockerUser;
+            StudentUser = studentUser;
+        }
+
         public string blocker { get; set; }
+        public string blocked { get; set; }
+        public string BlockerUser { get; set; }
+        public string StudentUser { get; set; }
+
+        public static void Add(BlockedAccounts newAccounts)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static BlockedAccounts FirstOrDefault(Func<object, bool> value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void Remove(BlockedAccounts accounts)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
