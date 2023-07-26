e687db315ce3ba4c3467288f7bd62bd38e196d37
Lorrea Ann C. Hugo <118536877+Lorreaann27@users.noreply.github.com>
2023-07-26 16:54:21 +0800

    Update Suggestions.cs

--- FlBlRules/Suggestions.cs
+++ FlBlRules/Suggestions.cs
@@ -1,15 +1,52 @@
-﻿using System;
+ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FlBlData;
+using FlBlModel;
 
 namespace FlBlRules
 {
-    internal class Suggestions
+    public class Suggestions
     {
-        public Suggestions(string myAccount) {
-            int studentNumber;
-        }
+       
+    private List<User> users;
+
+    public FriendSuggester()
+    {
+        users = new List<User>();
     }
+
+
+    public List<string> GetFriendSuggestions(string targetUser)
+    {
+        var targetUserObj = users.FirstOrDefault(u => u.Username == targetUser);
+        if (targetUserObj == null)
+        {
+            Console.WriteLine($"User '{targetUser}' not found.");
+            return new List<string>();
+        }
+
+        var targetFriends = targetUserObj.Friends;
+        var targetInterests = targetUserObj.Interests;
+
+        var suggestions = new List<string>();
+        foreach (var user in users)
+        {
+            if (user.Username != targetUser && !targetFriends.Contains(user.Username))
+            {
+                
+                var commonInterests = user.Interests.Intersect(targetInterests)
+                if (commonInterests.Any())
+                {
+                    suggestions.Add(user.Username);
+                }
+            }
+        }
+
+        return suggestions;
+    }
+}
+
 }
