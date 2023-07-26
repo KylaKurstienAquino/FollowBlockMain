ddc597f3b85e8458af5c7d497a46896b9d476b71
Claris Batacandolo <102318568+Yhesa@users.noreply.github.com>
2023-07-26 14:38:54 +0800

    arranging some codes

--- FlBlRules/Block.cs
+++ FlBlRules/Block.cs
@@ -1,34 +1,71 @@
-﻿using FlBlData;
-using FlBlModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using FlBlData;
+using FlBlModel;
 
 namespace FlBlRules
 {
-    public class Block
+    public  class Block
     {
-        private BlockData blockData;
+        private BlockData blockData;    
 
-        public Block()
-        {
-
+        public Block() {
+        
             blockData = new BlockData();
         }
 
-        public void BlockAcc(StudentAccount blocker, StudentAccount studentUser)
-        {
+        public void BlockAcc(StudentAccount block, StudentAccount studentUser) {
 
-            blockData.dataBlock(blocker.user, studentUser.user);
+            blockData.dataBlock(follower.user, studentUser.user);
         }
 
-        public void UnblockAcc(StudentAccount blocker, StudentAccount studentUser)
+        public void BlockerAcc(StudentAccount block, StudentAccount studentUser)
         {
 
-            blockData.dataUnblock(blocker.user, studentUser.user);
+            blockerData.dataBlocker(block.user, studentUser.user);
         }
     }
 }
+
+public class BlockData
+{
+    private List<BlockedAccounts> blockedAccounts;
+
+    public BlockData()
+    {
+        BlockedAccounts = new List<BlockedAccounts>();
+    }
+
+    public void dataBlock(string blockerUser, string studentUser)
+    {
+        if (blockedAccounts.Any(b => b.BlockerUser == blockerUser && b.StudentUser == studentUser))
+        {
+            Console.WriteLine("This user has been blocked to your account.");
+            return;
+        }
+
+        BlockedAccounts newAccounts = new BlockedAccounts(blockerUser, studentUser);
+        BlockedAccounts.Add(newAccounts);
+
+        Console.WriteLine($"User '{studentUser}' is blocked .");
+    }
+
+    public void dataUnblock(string blockerUser, string studentUser)
+    {
+        BlockedAccounts accounts = blockedAccounts.FirstOrDefault(b => b.BlockerUser == blockerUser && b.StudentUser == studentUser);
+        
+        if (accounts != null)
+        {
+            BlockedAccounts.Remove(accounts);
+            Console.WriteLine($"User '{blockerUser}' has unblocked '{studentUser}'.");
+        }
+        else
+        {
+            Console.WriteLine("You haven't blocked this user yet.");
+        }
+    }
+}
+
