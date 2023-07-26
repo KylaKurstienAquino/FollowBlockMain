11023f77c322852f6fe3be217028ab1fb523ebb7
Claris Batacandolo <102318568+Yhesa@users.noreply.github.com>
2023-07-26 17:56:45 +0800

    update blocked account

--- FlBlModel/BlockedAccounts.cs
+++ FlBlModel/BlockedAccounts.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace FlBlModel
 {
-    public class BlockededAccounts
+    public class BlockedAccounts
     {
         public BlockedAccounts(string blockerUser, string studentUser)
         {
