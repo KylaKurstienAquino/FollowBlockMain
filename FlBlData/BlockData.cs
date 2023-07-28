d7259be483c14e32e7eab4c2a8ce4f45078ebf73
Claris Batacandolo <102318568+Yhesa@users.noreply.github.com>
2023-07-29 02:04:48 +0800

    update some codes

--- FlBlData/BlockData.cs
+++ FlBlData/BlockData.cs
@@ -9,11 +9,31 @@ namespace FlBlData
 {
     public class BlockData
     {
-        public void dataBlock (string toblock, string myAccount) { 
+        public void dataBlock (string toblock, string myAccount) {
 
-            /*BlockedAccounts blockedAccounts = new();
-            blockedAccounts.blocker = myAccount;
-            blockedAccounts.blocked = toblock;*/
+            static SqlData sqlDataAccess = new();
+            static List<Blocked> Block = new();
+
+            public void ShowBlockedList(string accountBlocked)
+            {
+                Block = sqlDataAccess.GetBlockedList(accountBlocked);
+
+                if (Block.Count == 0)
+                {
+                    Console.WriteLine("No blocked list to show.");
+                }
+                else if (Block.Count > 0)
+                {
+                    foreach (var blocked in Block)
+                    {
+                        Console.WriteLine("StudentNo: " + blocked.StudentNo);
+                        Console.WriteLine("Username: " + blocked.BlockedName);
+                        Console.WriteLine("Course: " + blocked.BlockedCourse);
+                        Console.WriteLine("Section: " + blocked.BlockedSection);
+                        Console.WriteLine();
+                    }
+                }
+            }
 
         }
 
