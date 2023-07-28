723aaee5e1f5dfb6cc498b11933624abe4957013
Claris Batacandolo <118536877+Lorreaann27@users.noreply.github.com>
2023-07-29 06:08:17 +0800

    Update SuggestData.cs

--- FlBlData/SuggestData.cs
+++ FlBlData/SuggestData.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FlBlModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,23 @@ namespace FlBlData
 {
     internal class SuggestData
     {
+
+
+        static SqlData sqlDataAccess = new();
+        static List<Accounts> Account = new();
+
+        public void ShowSuggestions(string course, string section, string studentNo)
+        {
+            Account = sqlDataAccess.GetCourseSection(course, section, studentNo);
+
+            foreach (var account in Account)
+            {
+                Console.WriteLine($"\nUsername: {account.Username}");
+                Console.WriteLine($"Course: {account.Course}");
+                Console.WriteLine($"Section: {account.Section}");
+            }
+
+        }
+
     }
 }
